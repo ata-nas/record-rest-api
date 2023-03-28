fbff0640685978e95e9e6bae87500fa0e9656eb4
Atanas Atanasov <94196097+ata-nas@users.noreply.github.com>
2023-03-29 01:33:02 +0300

    some refactoring

--- src/main/java/com/rewedigital/medicalrecord/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/rewedigital/medicalrecord/service/impl/AppointmentServiceImpl.java
@@ -7,6 +7,7 @@ import com.rewedigital.medicalrecord.model.dto.appointment.UpdateAppointmentDTO;
 import com.rewedigital.medicalrecord.model.entity.AppointmentEntity;
 import com.rewedigital.medicalrecord.model.mapper.AppointmentMapper;
 import com.rewedigital.medicalrecord.repository.AppointmentRepository;
+import com.rewedigital.medicalrecord.repository.PricingHistoryRepository;
 import com.rewedigital.medicalrecord.service.AppointmentService;
 
 import jakarta.transaction.Transactional;
@@ -22,6 +23,14 @@ import java.util.List;
 public class AppointmentServiceImpl implements AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
+    /**
+     * This PricingHistoryRepository is here because in this context it is tightly coupled to Appointments.
+     * I removed Healthcare agency since not needed for context of app.
+     * This pricing history only supports adding and deleting. I prefer to not be able to update but to delete and enter
+     * data again since this is very static data and is used to preserve state of price overtime.
+     */
+    private final PricingHistoryRepository pricingHistoryRepository;
+    // TODO make functionality for this! Already have annotations for overlapping.
     private final AppointmentMapper appointmentMapper;
 
     @Override
