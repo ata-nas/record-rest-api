c23f0949a2de9d0e6101b0999bb9eebed8d65885
Atanas Atanasov <94196097+ata-nas@users.noreply.github.com>
2023-04-01 08:00:47 +0300

    adding more tests

--- src/test/java/com/rewedigital/medicalrecord/service/PatientServiceTest.java
+++ src/test/java/com/rewedigital/medicalrecord/service/PatientServiceTest.java
@@ -4,21 +4,26 @@ import com.rewedigital.medicalrecord.exception.notfound.NoSuchPatientEntityFound
 import com.rewedigital.medicalrecord.model.dto.patient.CreatePatientDTO;
 import com.rewedigital.medicalrecord.model.dto.patient.PatientDTO;
 import com.rewedigital.medicalrecord.model.dto.patient.UpdatePatientDTO;
-import com.rewedigital.medicalrecord.model.entity.AppointmentEntity;
+import com.rewedigital.medicalrecord.model.dto.stats.PatientIncomeDTO;
+import com.rewedigital.medicalrecord.model.dto.stats.PatientVisitDTO;
+import com.rewedigital.medicalrecord.model.dto.stats.PercentageInsuredPatientDTO;
 import com.rewedigital.medicalrecord.model.entity.PatientEntity;
 import com.rewedigital.medicalrecord.model.mapper.PatientMapper;
 import com.rewedigital.medicalrecord.repository.PatientRepository;
-import com.rewedigital.medicalrecord.service.impl.AppointmentServiceImpl;
 import com.rewedigital.medicalrecord.service.impl.PatientServiceImpl;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
 
@@ -191,6 +196,129 @@ class PatientServiceTest {
                 .isThrownBy(() -> toTest.update("wrong value", new UpdatePatientDTO()));
     }
 
+    @Test
+    public void testGetAllPatientsCurrentlyInsured_ReturnsCollectionWithAllPatientsDTOThatAreCurrentlyInsured() {
+        when(patientRepository.findAllCurrentlyInsured(LocalDate.now()))
+                .thenReturn(Set.of(new PatientEntity()));
 
+        when(patientMapper.allToDTO(patientRepository.findAllCurrentlyInsured(LocalDate.now())))
+                .thenReturn(Set.of(new PatientDTO()));
+
+        Set<PatientDTO> actual = toTest.getAllPatientsCurrentlyInsured();
+
+        assertThatCollection(actual).isNotNull();
+        assertThatCollection(actual).isNotEmpty();
+        assertThatCollection(actual).doesNotContainNull();
+        assertThatCollection(actual).hasSize(1);
+        assertThat(actual.iterator().next()).isExactlyInstanceOf(PatientDTO.class);
+    }
+
+    @Test
+    public void testGetAllPatientsCurrentlyInsured_ThrowsNoSuchPatientEntityFoundExceptionIfEntityNotFound() {
+        assertThatExceptionOfType(NoSuchPatientEntityFoundException.class)
+                .isThrownBy(() -> toTest.getAllPatientsCurrentlyInsured());
+    }
+
+    @Test
+    public void testGetAllPatientsCurrentlyNotInsured_ReturnsCollectionWithAllPatientsDTOThatAreCurrentlyNotInsured() {
+        when(patientRepository.findAllCurrentlyNotInsured(LocalDate.now()))
+                .thenReturn(Set.of(new PatientEntity()));
+
+        when(patientMapper.allToDTO(patientRepository.findAllCurrentlyNotInsured(LocalDate.now())))
+                .thenReturn(Set.of(new PatientDTO()));
+
+        Set<PatientDTO> actual = toTest.getAllPatientsCurrentlyNotInsured();
+
+        assertThatCollection(actual).isNotNull();
+        assertThatCollection(actual).isNotEmpty();
+        assertThatCollection(actual).doesNotContainNull();
+        assertThatCollection(actual).hasSize(1);
+        assertThat(actual.iterator().next()).isExactlyInstanceOf(PatientDTO.class);
+    }
+
+    @Test
+    public void testGetAllPatientsCurrentlyNotInsured_ThrowsNoSuchPatientEntityFoundExceptionIfEntityNotFound() {
+        assertThatExceptionOfType(NoSuchPatientEntityFoundException.class)
+                .isThrownBy(() -> toTest.getAllPatientsCurrentlyNotInsured());
+    }
+
+    @Test
+    public void testGetPercentageCurrentlyInsured_ReturnsPercentOfCurrentlyInsuredPatients() {
+        when(patientRepository.findAllByDeletedFalse())
+                .thenReturn(Set.of(new PatientEntity()));
+
+        when(patientRepository.countAllCurrentlyInsured(LocalDate.now()))
+                .thenReturn(1L);
+
+        PercentageInsuredPatientDTO actual = toTest.getPercentageCurrentlyInsured();
+
+        assertThat(actual.getPercentage().doubleValue()).isEqualTo(100.0);
+        assertThat(actual).isExactlyInstanceOf(PercentageInsuredPatientDTO.class);
+    }
+
+    @Test
+    public void testGetPercentageCurrentlyNotInsured_ReturnsPercentOfCurrentlyNotInsuredPatients() {
+        when(patientRepository.findAllByDeletedFalse())
+                .thenReturn(Set.of(new PatientEntity()));
+
+        when(patientRepository.countAllCurrentlyNotInsured(LocalDate.now()))
+                .thenReturn(1L);
+
+        PercentageInsuredPatientDTO actual = toTest.getPercentageCurrentlyNotInsured();
+
+        assertThat(actual.getPercentage().doubleValue()).isEqualTo(100.0);
+        assertThat(actual).isExactlyInstanceOf(PercentageInsuredPatientDTO.class);
+    }
+
+    @Test
+    public void testGetPatientVisitCount_ReturnsTheCountOfVisitsByAPatient() {
+        PatientVisitDTO patientVisitDTO = new PatientVisitDTO();
+        ReflectionTestUtils.setField(patientVisitDTO, "countVisits", 10L);
+
+        when(patientRepository.countVisitsByPatientUic("1"))
+                .thenReturn(10L);
+
+        when(patientMapper.toDTO(patientRepository.countVisitsByPatientUic("1")))
+                .thenReturn(patientVisitDTO);
+
+        PatientVisitDTO actual = toTest.getPatientVisitCount("1");
+
+        assertThat(actual.getCountVisits()).isEqualTo(10L);
+        assertThat(actual).isExactlyInstanceOf(PatientVisitDTO.class);
+    }
+
+    @Test
+    public void testGetPatientsIncomeFromInsured_ReturnsTotalIncomeMadeFromPatientsWhoWereInsuredAtTimeOfVisit() {
+        PatientIncomeDTO patientIncomeDTO = new PatientIncomeDTO();
+        ReflectionTestUtils.setField(patientIncomeDTO, "income", BigDecimal.TEN);
+
+        when(patientRepository.totalIncomeFromInsured())
+                .thenReturn(BigDecimal.TEN);
+
+        when(patientMapper.toDTO(patientRepository.totalIncomeFromInsured()))
+                .thenReturn(patientIncomeDTO);
+
+        PatientIncomeDTO actual = toTest.getPatientsIncomeFromInsured();
+
+        assertThat(actual.getIncome().doubleValue()).isEqualTo(10.0);
+        assertThat(actual).isExactlyInstanceOf(PatientIncomeDTO.class);
+    }
+
+    @Test
+    public void testGetPatientsIncomeFromNotInsured_ReturnsTotalIncomeMadeFromPatientsWhoWereNotInsuredAtTimeOfVisit() {
+        PatientIncomeDTO patientIncomeDTO = new PatientIncomeDTO();
+        ReflectionTestUtils.setField(patientIncomeDTO, "income", BigDecimal.TEN);
+
+        when(patientRepository.totalIncomeFromNotInsured())
+                .thenReturn(BigDecimal.TEN);
+
+        when(patientMapper.toDTO(patientRepository.totalIncomeFromNotInsured()))
+                .thenReturn(patientIncomeDTO);
+
+        PatientIncomeDTO actual = toTest.getPatientsIncomeFromNotInsured();
+
+        assertThat(actual.getIncome().doubleValue()).isEqualTo(10.0);
+        assertThat(actual).isExactlyInstanceOf(PatientIncomeDTO.class);
+    }
 
 }
